884cde1a997d85a80c61074b0e255714e9266a44
joshua-rigg <joshua.rigg@stu.ocsb.ca>
2026-03-26 21:55:02 -0400

    Changed comments

--- main.ts
+++ main.ts
@@ -5,4 +5,50 @@
  * This program ...
 */
 
-basic.showString('Hello, World!')
+// variable
+let neopixelStrip: neopixel.Strip = null
+
+// clear screen
+basic.clearScreen()
+
+neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+
+// turn all 4 pixels off
+neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.show()
+
+// show happyface
+basic.showIcon(IconNames.Happy)
+
+// when button "a" is pressed
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    basic.showIcon(IconNames.Yes)
+
+    // turn on Green light and wait for 1 second
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+    neopixelStrip.show()
+    basic.pause(1000)
+
+    // turn off Green light and turn on Yellow light and wait for 0.5 seconds
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
+    neopixelStrip.show()
+    basic.pause(500)
+
+    // turn off Yellow light and turn on Red light and wait for 1 second
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+    neopixelStrip.show()
+    basic.pause(1000)
+
+    // turn off Red light
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.show()
+
+    // clear screen
+    basic.clearScreen()
+})
