9a7547c98371003d1b107c5679d2fdcc88709260
joshua-rigg <joshua.rigg@stu.ocsb.ca>
2026-04-02 13:19:35 -0400

    changed comments

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Joshua
+ * Created on: feb 2026
+ * This program will show traffic light colors 
 */
 
 // variable
